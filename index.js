0b4547bfb6803577cb5adf2221b598a204183df1
daphnemagsby <daphnemagsby@gmail.com>
2021-09-06 14:50:16 -0500

    Set view engine ejs

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = new express();
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
 	res.sendFile(path.resolve(__dirname, './pages/index.html'));
+});
+
+app.get('/about', (req,res)=>{
+	res.sendFile(path.resolve(__dirname, './pages/about.html'));
+});
+
+app.get('/contact', (req,res)=>{
+	res.sendFile(path.resolve(__dirname, './pages/contact.html'));
+});
+
+app.get('/sample', (req,res)=>{
+	res.sendFile(path.resolve(__dirname, './pages/sample.html'));
 });
 
 app.listen(5000, ()=>{
